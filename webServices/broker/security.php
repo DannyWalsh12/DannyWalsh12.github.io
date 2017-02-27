5459cf4ca3cc11dc2355a582613cfa52ed28d238
Danny Walsh <dwalsh12@mail.bw.edu>
2017-02-27 12:34:00 -0500

    added the DB file as well as making authenticate working properly. still working on creating user and adding it to the database,

--- webServices/broker/security.php
+++ webServices/broker/security.php
@@ -4,20 +4,46 @@
  * Created by PhpStorm.
  * User: briankrupp
  */
+
+require_once ("db.php");
+
 class securityBroker
 {
   function authenticate($request) {
-    // Create default message
-    $response["authenticationStatus"] = "You failed to login";
 
-    //checks for username
-    if($request->username == "admin") {
-        // Check for password
-        if ($request->secretPassword == "password") {
-            $response["authenticationStatus"] = "username= ".$request->username."<br />password= ".$request->secretPassword;
-        }
+    $db = new db();
+
+    $userId = $db->authenticateUser($request->username, $this->hashPass($request->secretPassword));
+
+    $response["userId"]=-1;
+
+    if($userId > 0){
+        $response["userId"] = $userId;
+        $_SESSION["userId"] = $userId;
     }
 
+    return $response;
+  }
+
+  function createUser($request){
+    $db = new db();
+
+    $response["createUser"] = "no User Created";
+
+    if($db->createUserAccount($request->username, $this->hashPass($request->password), $request->Email,$request->Verification)){
+        $response["createUser"] = "User Created";
         return $response;
+    }
+    $response["createUser"] = "User failed to be created";
+    return $response;
+  }
+
+  function logOff($request){
+      session_unset();
+      session_destroy();
+  }
+
+  function hashPass($password){
+      return hash("sha256",$password);
   }
 }
